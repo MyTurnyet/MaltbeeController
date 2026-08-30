6c91bddd395a0fbea596558b1b53b9b8bbcc95a3
Paige Watson <paige.watson@outlook.com>
2026-08-30 09:05:00 -0700

    ^ F Add CaptivePortalServer

---
+++ lib/McsEsp32/src/adapters/CaptivePortalServer.cpp
@@ -0,0 +1,60 @@
+#ifdef ARDUINO
+
+#include "CaptivePortalServer.h"
+
+#include "../domain/NodeConfig.h"
+
+CaptivePortalServer::CaptivePortalServer(WebFormCommissioningAdapter& adapter) : adapter_(adapter)
+{
+}
+
+void CaptivePortalServer::begin(const std::string& apName)
+{
+    WiFi.softAP(apName.c_str());
+
+    IPAddress apIp = WiFi.softAPIP();
+    dnsServer_.start(53, "*", apIp);
+
+    webServer_.on("/", [this]() { handleRoot(); });
+    webServer_.on("/submit", HTTP_POST, [this]() { handleSubmit(); });
+    webServer_.onNotFound([this]() { handleRoot(); });
+    webServer_.begin();
+}
+
+void CaptivePortalServer::poll()
+{
+    dnsServer_.processNextRequest();
+    webServer_.handleClient();
+}
+
+void CaptivePortalServer::handleRoot()
+{
+    webServer_.send(200, "text/html", SetupFormRenderer::render(adapter_.currentValues()).c_str());
+}
+
+void CaptivePortalServer::handleSubmit()
+{
+    const WebFormSubmission form = readForm();
+    const std::string response = adapter_.submit(form);
+    webServer_.send(200, "text/plain", response.c_str());
+}
+
+WebFormSubmission CaptivePortalServer::readForm()
+{
+    WebFormSubmission form;
+    form.nodeId = webServer_.arg("id").c_str();
+    form.wifiSsid = webServer_.arg("wifi_ssid").c_str();
+    form.wifiPassword = webServer_.arg("wifi_password").c_str();
+    form.brokerHost = webServer_.arg("broker_host").c_str();
+    form.brokerPort = webServer_.arg("broker_port").c_str();
+
+    for (int i = 0; i < NodeConfig::kChannelCount; ++i)
+    {
+        const std::string fieldName = "t" + std::to_string(i + 1) + "_name";
+        form.channelJmriNames[i] = webServer_.arg(fieldName.c_str()).c_str();
+    }
+
+    return form;
+}
+
+#endif
---
+++ lib/McsEsp32/src/adapters/CaptivePortalServer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifdef ARDUINO
+
+#include <DNSServer.h>
+#include <WebServer.h>
+#include <WiFi.h>
+
+#include <string>
+
+#include "WebFormCommissioningAdapter.h"
+#include "../domain/SetupFormRenderer.h"
+
+class CaptivePortalServer
+{
+public:
+    explicit CaptivePortalServer(WebFormCommissioningAdapter& adapter);
+
+    void begin(const std::string& apName);
+    void poll();
+
+private:
+    void handleRoot();
+    void handleSubmit();
+    WebFormSubmission readForm();
+
+    WebFormCommissioningAdapter& adapter_;
+    DNSServer dnsServer_;
+    WebServer webServer_{80};
+};
+
+#endif
