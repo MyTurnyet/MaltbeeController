b4f7ada785af5b3c43a945ba7f1be09789b90480
Paige Watson <paige.watson@outlook.com>
2026-08-31 12:04:49 -0700

    f - add WiFi wait and feedback

--- lib/McsEsp32/src/adapters/WiFiLink.cpp
+++ lib/McsEsp32/src/adapters/WiFiLink.cpp
@@ -2,13 +2,11 @@
 
 #include "WiFiLink.h"
 
-WiFiLink::WiFiLink(Clock& clock, const unsigned long retryIntervalMs)
-    : clock_(clock), retryIntervalMs_(retryIntervalMs)
-{
+WiFiLink::WiFiLink(Clock &clock, const unsigned long retryIntervalMs)
+    : clock_(clock), retryIntervalMs_(retryIntervalMs) {
 }
 
-void WiFiLink::begin(const std::string& ssid, const std::string& password)
-{
+void WiFiLink::begin(const std::string &ssid, const std::string &password) {
     ssid_ = ssid;
     password_ = password;
 
@@ -21,28 +19,32 @@ void WiFiLink::begin(const std::string& ssid, const std::string& password)
     connect();
 }
 
-void WiFiLink::poll()
-{
-    if (WiFi.status() == WL_CONNECTED)
-    {
+void WiFiLink::poll() {
+    if (WiFi.status() == WL_CONNECTED) {
         return;
     }
 
-    if (clock_.nowMilliseconds() - lastAttemptMs_ >= retryIntervalMs_)
-    {
+    if (clock_.nowMilliseconds() - lastAttemptMs_ >= retryIntervalMs_) {
         connect();
     }
 }
 
-bool WiFiLink::connected() const
-{
+bool WiFiLink::connected() const {
     return WiFi.status() == WL_CONNECTED;
 }
 
-void WiFiLink::connect()
-{
+void WiFiLink::connect() {
     WiFi.begin(ssid_.c_str(), password_.c_str());
     lastAttemptMs_ = clock_.nowMilliseconds();
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.print("WiFi connected, IP: ");
+    Serial.println(WiFi.localIP());
 }
 
 #endif
